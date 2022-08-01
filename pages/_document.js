cdfeffe9fe25a3cce42dd9af472c4882537e95e8
shafin <shafinshahriar340@gmail.com>
2022-08-02 00:27:01 +0600

    google analytics added

---
+++ pages/_document.js
@@ -0,0 +1,33 @@
+import { Head, Html, Main, NextScript } from 'next/document';
+import Script from 'next/script';
+
+export default function Document() {
+	return (
+		<Html lang='en'>
+			<Head>
+				<link rel='icon' href='/favicon.ico' />
+				<Script
+					src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TRACKING_ID}`}
+					strategy='afterInteractive'
+				/>
+
+				<Script id='google-analytics' strategy='afterInteractive'>
+					{`
+                      window.dataLayer = window.dataLayer || [];
+                      function gtag(){window.dataLayer.push(arguments);}
+                      gtag('js', new Date());
+
+                      gtag('config', '${process.env.NEXT_PUBLIC_GA_TRACKING_ID}', {
+                          page_path: window.location.pathname,
+                      });
+                 `}
+				</Script>
+			</Head>
+
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
+}
