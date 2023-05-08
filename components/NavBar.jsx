d43881039684ee3bd9dea632e12d2ca9d5fa0bb9
Shahriar Shafin <shafinshahriar340@gmail.com>
2023-05-08 11:14:51 +0000

    fixed 12

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -3,9 +3,10 @@ import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { FaGithub } from 'react-icons/fa';
 import UserImg from '../public/assets/images/icons/icon-565x565.png';
-
+import { useState } from 'react';
 const NavBar = () => {
 	const router = useRouter();
+	const [isMenuOpen, setIsMenuOpen] = useState(false);
 
 	const webLinks = [
 		{ name: 'About', path: '/about' },
@@ -20,7 +21,10 @@ const NavBar = () => {
 					<div className='flex items-center w-full'>
 						<div className='w-2/4 justify-start items-center inline-flex'>
 							<div className='dropdown'>
-								<label tabIndex={0} className='lg:hidden'>
+								<label
+									onClick={() => setIsMenuOpen(!isMenuOpen)}
+									className='lg:hidden'
+								>
 									<svg
 										xmlns='http://www.w3.org/2000/svg'
 										className='bg-sh-dark p-2 rounded-lg h-9 w-9 mr-5 cursor-pointer'
@@ -36,26 +40,25 @@ const NavBar = () => {
 										/>
 									</svg>
 								</label>
-								<ul
-									tabIndex={0}
-									className='border border-sh-white-500 bg-sh-dark-500 bg-opacity-[0.97] backdrop-filter backdrop-blur-3xl shadow-lg shadow-sh-dark-500 rounded-md dropdown-content mt-3 px-2 py-5 w-64 space-y-5'
-								>
-									{webLinks.map((link, index) => (
-										<li key={index}>
-											<Link href={link.path}>
-												<a
-													className={
-														router.pathname == link.path
-															? 'menu-item active'
-															: 'menu-item'
-													}
-												>
-													{link.name}
-												</a>
-											</Link>
-										</li>
-									))}
-								</ul>
+								{isMenuOpen && (
+									<ul className='border border-sh-white-500 bg-sh-dark-500 bg-opacity-[0.97] backdrop-filter backdrop-blur-3xl shadow-lg shadow-sh-dark-500 rounded-md absolute mt-3 px-2 py-5 w-64 space-y-5'>
+										{webLinks.map((link, index) => (
+											<li key={index} onClick={() => setIsMenuOpen(false)}>
+												<Link href={link.path}>
+													<a
+														className={
+															router.pathname == link.path
+																? 'menu-item active'
+																: 'menu-item'
+														}
+													>
+														{link.name}
+													</a>
+												</Link>
+											</li>
+										))}
+									</ul>
+								)}
 							</div>
 							<Link href='/'>
 								<a className='rounded-full border-2 border-sh-blue flex mr-7 w-10 h-10 scale-90 hover:scale-100 transition ease-in select-none'>
