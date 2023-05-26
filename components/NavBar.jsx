8fea96ebb06c6102c9f86e45fa9f1501e7195a37
shafin <shafinshahriar340@gmail.com>
2023-05-27 00:36:54 +0600

    pwa capabilities added

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -15,66 +15,34 @@ const NavBar = () => {
 	];
 
 	return (
-		<>
-			<div className='bg-sh-dark-500 bg-opacity-80 sticky top-0 z-20 backdrop-filter backdrop-blur-sm shadow-lg'>
-				<div className='max-w-[50rem] mx-auto w-full px-5 py-2.5 md:px-10 lg:px-0'>
-					<div className='flex items-center w-full'>
-						<div className='w-2/4 justify-start items-center inline-flex'>
-							<div className='dropdown'>
-								<label
-									onClick={() => setIsMenuOpen(!isMenuOpen)}
-									className='lg:hidden'
+		<div className='bg-sh-dark-500 bg-opacity-80 sticky top-0 z-20 backdrop-filter backdrop-blur-sm shadow-lg'>
+			<div className='max-w-[50rem] mx-auto w-full px-5 py-2.5 md:px-10 lg:px-0'>
+				<div className='flex items-center w-full'>
+					<div className='w-2/4 justify-start items-center inline-flex'>
+						<div className='dropdown'>
+							<label
+								onClick={() => setIsMenuOpen(!isMenuOpen)}
+								className='lg:hidden'
+							>
+								<svg
+									xmlns='http://www.w3.org/2000/svg'
+									className='bg-sh-dark p-2 rounded-lg h-9 w-9 mr-5 cursor-pointer'
+									fill='none'
+									viewBox='0 0 24 24'
+									stroke='currentColor'
 								>
-									<svg
-										xmlns='http://www.w3.org/2000/svg'
-										className='bg-sh-dark p-2 rounded-lg h-9 w-9 mr-5 cursor-pointer'
-										fill='none'
-										viewBox='0 0 24 24'
-										stroke='currentColor'
-									>
-										<path
-											strokeLinecap='round'
-											strokeLinejoin='round'
-											strokeWidth={2}
-											d='M4 6h16M4 12h8m-8 6h16'
-										/>
-									</svg>
-								</label>
-								{isMenuOpen && (
-									<ul className='border border-sh-white-500 bg-sh-dark-500 bg-opacity-[0.97] backdrop-filter backdrop-blur-3xl shadow-lg shadow-sh-dark-500 rounded-md absolute mt-3 px-2 py-5 w-64 space-y-5'>
-										{webLinks.map((link, index) => (
-											<li key={index} onClick={() => setIsMenuOpen(false)}>
-												<Link href={link.path}>
-													<a
-														className={
-															router.pathname == link.path
-																? 'menu-item active'
-																: 'menu-item'
-														}
-													>
-														{link.name}
-													</a>
-												</Link>
-											</li>
-										))}
-									</ul>
-								)}
-							</div>
-							<Link href='/'>
-								<a className='rounded-full border-2 border-sh-blue flex mr-7 w-10 h-10 scale-90 hover:scale-100 transition ease-in select-none'>
-									<Image
-										className='rounded-full'
-										src={UserImg}
-										alt='Shafin Avatar'
-										objectFit='contain'
-										draggable='false'
+									<path
+										strokeLinecap='round'
+										strokeLinejoin='round'
+										strokeWidth={2}
+										d='M4 6h16M4 12h8m-8 6h16'
 									/>
-								</a>
-							</Link>
-							<div className='hidden lg:flex'>
-								<ul className='inline-flex flex-row gap-3'>
+								</svg>
+							</label>
+							{isMenuOpen && (
+								<ul className='border border-sh-white-500 bg-sh-dark-500 bg-opacity-[0.97] backdrop-filter backdrop-blur-3xl shadow-lg shadow-sh-dark-500 rounded-md absolute mt-3 px-2 py-5 w-64 space-y-5'>
 									{webLinks.map((link, index) => (
-										<li key={index}>
+										<li key={index} onClick={() => setIsMenuOpen(false)}>
 											<Link href={link.path}>
 												<a
 													className={
@@ -89,23 +57,54 @@ const NavBar = () => {
 										</li>
 									))}
 								</ul>
-							</div>
+							)}
 						</div>
-
-						<div className='w-2/4 justify-end text-xl flex gap-3'>
-							<a
-								target='_blank'
-								rel='noopener noreferrer'
-								href='https://github.com/shahriarshafin'
-								className='cursor-pointer p-3 rounded-xl hover:bg-sh-dark transition ease-in'
-							>
-								<FaGithub />
+						<Link href='/'>
+							<a className='rounded-full border-2 border-sh-blue flex mr-7 w-10 h-10 scale-90 hover:scale-100 transition ease-in select-none'>
+								<Image
+									className='rounded-full'
+									src={UserImg}
+									alt='Shafin Avatar'
+									objectFit='contain'
+									draggable='false'
+								/>
 							</a>
+						</Link>
+						<div className='hidden lg:flex'>
+							<ul className='inline-flex flex-row gap-3'>
+								{webLinks.map((link, index) => (
+									<li key={index}>
+										<Link href={link.path}>
+											<a
+												className={
+													router.pathname == link.path
+														? 'menu-item active'
+														: 'menu-item'
+												}
+											>
+												{link.name}
+											</a>
+										</Link>
+									</li>
+								))}
+							</ul>
 						</div>
+					</div>
+
+					<div className='w-2/4 justify-end text-xl flex gap-3'>
+						<a
+							target='_blank'
+							aria-label='github link'
+							rel='noopener noreferrer'
+							href='https://github.com/shahriarshafin'
+							className='cursor-pointer p-3 rounded-xl hover:bg-sh-dark transition ease-in'
+						>
+							<FaGithub />
+						</a>
 					</div>
 				</div>
 			</div>
-		</>
+		</div>
 	);
 };
 
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import { PageLayout } from '../components';
 import '../styles/globals.css';
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }) {
+	useEffect(() => {
+		if ('serviceWorker' in navigator) {
+			navigator.serviceWorker.register('/service-worker.js');
+		}
+	}, []);
+
 	return (
 		<PageLayout>
 			<Component {...pageProps} />
