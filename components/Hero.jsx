377557f07e11a9ea962be05c355cbdd8ed354799
shafin <shafinshahriar340@gmail.com>
2023-08-15 03:13:27 +0600

    animated hero img

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,33 +1,60 @@
 import Image from 'next/image';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { greetingList } from '../data/greetings';
-import UserImg from '../public/assets/images/icons/icon-565x565.png';
+import UserImg from '../public/assets/images/icons/shafin-logo.png';
 
 const Hero = () => {
+	const typingIntervalRef = useRef(null);
 	const [arrItem, setArrItem] = useState(0);
+	const [typedText, setTypedText] = useState('');
+	const fullText = "Hey, It's so nice to see you here, thanks for visiting !";
 
 	const nextItem = () => {
-		const check = (arrItem + 1) % greetingList.length;
-		setArrItem(check);
+		setArrItem((arrItem + 1) % greetingList.length);
 	};
 
+	const handleMouseEnter = () => {
+		startTypingAnimation();
+	};
+
+	const handleMouseLeave = () => {
+		clearInterval(typingIntervalRef.current);
+		setTypedText('');
+	};
+
+	const startTypingAnimation = () => {
+		let index = 0;
+		typingIntervalRef.current = setInterval(() => {
+			setTypedText(fullText.substring(0, index));
+			index++;
+			if (index > fullText.length) {
+				clearInterval(typingIntervalRef.current);
+			}
+		}, 50);
+	};
 	return (
 		<section>
 			<div className='flex items-center justify-center md:flex-row flex-col'>
-				<div className='rounded-full border-[3px] border-sh-blue relative scale-95 hover:scale-100 transition ease-in shadow-lg'>
-					<div className='h-44 w-44 select-none relative group'>
-						<Image
-							className='rounded-full w-full h-full'
-							src={UserImg}
-							alt='ShafinAvatar'
-							layout='fill'
-							draggable='false'
-						/>
-						<div className='absolute bottom-0 flex-col items-center hidden mb-44 group-hover:flex'>
-							<span className='relative z-10 p-2 text-xs leading-none text-sh-white whitespace-no-wrap bg-sh-dark shadow-lg rounded-md'>
-								{"Hey, It's so nice to see you here, thanks for visiting !"}
-							</span>
-							<div className='w-3 h-3 -mt-2 rotate-45 bg-sh-dark'></div>
+				<div className='my-5'>
+					<div className='relative h-44 w-44 rounded-full bg-gradient-to-b bg-[#8e7fc9] hover:from-pink-500 hover:to-[#f5d498] flex justify-center items-center'>
+						<div
+							onMouseEnter={handleMouseEnter}
+							onMouseLeave={handleMouseLeave}
+							className='group absolute -top-[64px] transition-all duration-200 scale-100 hover:scale-110 ease-in'
+						>
+							<Image
+								src={UserImg}
+								draggable='false'
+								// layout='fill'
+								alt='ShafinAvatar'
+								className='drop-shadow-md'
+							/>
+							<div className='absolute bottom-0 flex-col items-center hidden w-32 -ml-24 mb-48 lg:group-hover:flex'>
+								<span className='p-2 w-32 h-[50px] relative z-10 text-xs leading-none text-sh-white whitespace-no-wrap bg-sh-dark shadow-lg rounded-md'>
+									{typedText}
+								</span>
+								<div className='before:content-[""] before:w-0 before:h-0 before:absolute before:border-r-[24px] before:border-r-sh-dark before:border-l-[12px] before:border-l-transparent before:border-t-[12px] before:border-t-sh-dark before:border-b-[20px] before:border-b-transparent before:right-3 before:-bottom-3'></div>
+							</div>
 						</div>
 					</div>
 				</div>
